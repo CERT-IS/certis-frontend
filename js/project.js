3c38e98710ea454ec6f57fea179374311175f257
bananacat <d34sda334@gmail.com>
2024-08-08 20:53:50 +0900

    style:replacing project content to card list

--- js/project.js
+++ js/project.js
@@ -4,19 +4,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function loadBoardData() {
   const data = JSON.parse(localStorage.getItem('project_boardData')) || [];
-  const tableBody = document.getElementById('board-body');
-  tableBody.innerHTML = ''; 
-  const reversedData = data.reverse();
-  reversedData.forEach((item, index) => {
-    const row = document.createElement('tr');
-    row.innerHTML = `
-    <td>${reversedData.length - index}</td>
-    <td>${item.제목}</td>
-    <td>${item.글쓴이}</td>
-    <td>${item.작성일}</td>`;
-    row.addEventListener('click', function() {
-      window.location.href = `project_data.html?index=${index}`;
+  const container = document.getElementById('board-container');
+  container.innerHTML = ''; 
+  
+  data.forEach((item, index) => {
+    const card = document.createElement('div');
+    card.className = 'card';
+    const imageUrl = item.image ? item.image : '../img/blackbug.png';
+    card.innerHTML = `
+    <img src="${imageUrl}" class="card-image">
+    <div class="card-content">
+      <h2>${item.제목}</h2>
+      <p>${item.작성일}</p>
+      </div>
+      `
+      card.addEventListener('click',function(){
+        window.location.href =  `project_data.html?index=${index}`;
+      });
+      container.appendChild(card);
     });
-    tableBody.appendChild(row);
-  });
 }
